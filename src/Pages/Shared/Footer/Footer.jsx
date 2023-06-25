225046d4b30f76ec2bb6f4a1cbcc1cb8f84f16fe
abcmehedi5 <abcmehedi5@gmail.com>
2023-06-26 00:50:58 +0600

    footer update

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,30 +1,54 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="footer p-10 bg-neutral text-neutral-content">
-        <div>
-          <span className="footer-title">Services</span> 
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
-        </div> 
-        <div>
-          <span className="footer-title">Company</span> 
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
-        </div> 
-        <div>
-          <span className="footer-title">Legal</span> 
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
-        </div>
-      </footer>
-    );
+  return (
+    <footer className="footer p-10 bg-neutral  justify-around text-neutral-content">
+      <div>
+        <span className="footer-title">Services</span>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </div>
+      <div>
+        <span className="footer-title">Company</span>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </div>
+      <div>
+        <span className="footer-title">Legal</span>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </div>
+      <div>
+        <span className="footer-title">Social</span>
+        <a
+          target="_blank"
+          href="https://www.facebook.com/mehedifb1/"
+          className="link link-hover"
+        >
+          Facebook
+        </a>
+        <a
+          target="_blank"
+          href="https://github.com/abcmehedi5"
+          className="link link-hover"
+        >
+          Github
+        </a>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/mehedilinkdin/"
+          className="link link-hover"
+        >
+          Linkdin
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
