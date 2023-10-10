52409c935629b16ccc0f21395f291b33685d8d95
abcmehedi5 <abcmehedi5@gmail.com>
2023-10-10 18:20:48 +0600

    redux configure

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './Routes/Routs.jsx'
-import 'swiper/css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routs.jsx";
+import "swiper/css";
+import { Provider } from "react-redux";
+import { store } from "./Redux/app/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
+  </React.StrictMode>
+);
